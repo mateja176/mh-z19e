d406affc260bc2e9a28177f7ecc01a3fd5137676
Mateja Petrovic <mateja176@gmail.com>
2025-04-06 11:20:17 +0200

    feat: implements basic getCO2 and getTemperature readings

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <MHZ19.h>
+#include <SoftwareSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int txPin = D1;
+const int rxPin = D2;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+SoftwareSerial softwareSerial(rxPin, txPin);
+
+MHZ19 mhz19;
+
+unsigned long elapsedMs = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("\nInitializing MH-Z19 sensor...");
+  // Start the software serial port for communication with the MH-Z19
+  softwareSerial.begin(9600);
+  // Initialize the MHZ19 library, passing the serial port object
+  mhz19.begin(softwareSerial); // MH-Z19 default baud rate
+  // Reference https://github.com/WifWaf/MH-Z19
+  mhz19.autoCalibration(); // Turn auto calibration ON (OFF autoCalibration(false))
+  Serial.println("MH-Z19 sensor initialized.");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  if (millis() - elapsedMs >= 2000)
+  {
+    elapsedMs = millis();
+    const int co2Ppm = mhz19.getCO2();
+    Serial.print("CO2 (ppm): ");
+    Serial.println(co2Ppm);
+    const float tempC = mhz19.getTemperature();
+    Serial.print("Temperature (C): ");
+    Serial.println(tempC);
+  }
 }
